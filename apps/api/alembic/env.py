933508e12779bbd17a84232d30f4b33607038b70
kuewout <kueweipoen@outlook.com>
2026-03-14 19:55:22 +0800

    /Users/aa/git/yt-down/apps/api

--- apps/api/alembic/env.py
+++ apps/api/alembic/env.py
@@ -5,7 +5,6 @@ from sqlalchemy import engine_from_config, pool
 
 from app.core.config import settings
 from app.db.base import Base
-from app.models import Playlist, Video
 
 config = context.config
 config.set_main_option("sqlalchemy.url", settings.database_url)
